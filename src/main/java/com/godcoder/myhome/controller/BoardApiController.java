115ebd8e11ee1d586ce8ba9a6822a6a83de80f76
braverokmc79 <braverokmc79@gmail.com>
2022-03-17 12:53:02 +0900

    #7. JPA이용한 RESTful API 작성

---
+++ src/main/java/com/godcoder/myhome/controller/BoardApiController.java
@@ -0,0 +1,69 @@
+package com.godcoder.myhome.controller;
+
+
+import com.godcoder.myhome.model.Board;
+import com.godcoder.myhome.repository.BoardRepository;
+import org.springframework.web.bind.annotation.*;
+import org.thymeleaf.util.StringUtils;
+
+import javax.persistence.EntityExistsException;
+import java.util.List;
+
+@RestController
+@RequestMapping("/api")
+class BoardApiController {
+
+    private final BoardRepository repository;
+
+    BoardApiController(BoardRepository repository) {
+        this.repository = repository;
+    }
+
+
+    // Aggregate root
+    // tag::get-aggregate-root[]
+    @GetMapping("/boards")
+    List<Board> all(@RequestParam(required = false, defaultValue = "") String title
+        , @RequestParam(required = false, defaultValue = "") String content) {
+
+        System.out.println("파라미터 : " + title);
+        if(StringUtils.isEmpty(title) && StringUtils.isEmpty(content)){
+            return repository.findAll();
+        }else{
+            return repository.findByTitleOrContent(title, content);
+        }
+    }
+
+    // end::get-aggregate-root[]
+
+    @PostMapping("/boards")
+    Board newBoard(@RequestBody Board newBoard) {
+        return repository.save(newBoard);
+    }
+
+    @GetMapping("/boards/{id}")
+    Board one(@PathVariable Long id) {
+        return repository.findById(id).orElseThrow(EntityExistsException::new);
+    }
+
+    @PutMapping("/boards/{id}")
+    Board replaceBoard(@RequestBody Board newBoard, @PathVariable Long id) {
+
+        return repository.findById(id)
+                .map(employee -> {
+                    employee.setTitle(newBoard.getTitle());
+                    employee.setContent(newBoard.getContent());
+                    return repository.save(employee);
+                })
+                .orElseGet(() -> {
+                    newBoard.setId(id);
+                    return repository.save(newBoard);
+                });
+    }
+
+    @DeleteMapping("/boards/{id}")
+    void deleteBoard(@PathVariable Long id) {
+        repository.deleteById(id);
+    }
+
+}
