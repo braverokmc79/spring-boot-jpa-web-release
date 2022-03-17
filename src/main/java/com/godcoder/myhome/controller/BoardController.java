28b3d28cde94fd56961b4f6d3342330caa198970
braverokmc79 <braverokmc79@gmail.com>
2022-03-17 20:02:41 +0900

    #8. JPA를 이용한 페이지 처리 및 검색

--- src/main/java/com/godcoder/myhome/controller/BoardController.java
+++ src/main/java/com/godcoder/myhome/controller/BoardController.java
@@ -4,6 +4,9 @@ import com.godcoder.myhome.model.Board;
 import com.godcoder.myhome.repository.BoardRepository;
 import com.godcoder.myhome.validator.BoardValidator;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -14,6 +17,7 @@ import org.thymeleaf.util.StringUtils;
 import javax.persistence.EntityExistsException;
 import javax.validation.Valid;
 import java.util.List;
+import java.util.Optional;
 
 @Controller
 @RequestMapping("/board")
@@ -25,12 +29,30 @@ public class BoardController {
     @Autowired
     private BoardValidator boardValidator;
 
+
     @GetMapping("/list")
-    public String list(Model model){
-        List<Board> boards= boardRepository.findAll(Sort.by(Sort.Direction.DESC, "id"));
+    public String list(Model model,
+                       @RequestParam(value = "page",defaultValue ="1", required = false) Optional<Integer> page,
+                     @RequestParam(required = false, defaultValue = "")  String searchText
+    ){
+      //  List<Board> boards= boardRepository.findAll(Sort.by(Sort.Direction.DESC, "id"));
+        int pageSize=3;
+        Sort sort1=Sort.by("id").descending();
+        Pageable pageable=PageRequest.of(page.isPresent() ? page.get()-1 :0, pageSize,  sort1);
+
+        //Page<Board> boards= boardRepository.findAll(pageable);
+        Page<Board> boards= boardRepository.findByTitleContainingOrContentContaining(searchText, searchText,pageable);
+        int startPage=Math.max(1, boards.getPageable().getPageNumber() - 4);
+        int endPage=Math.min(boards.getTotalPages(), boards.getPageable().getPageNumber()+4);
+
+        model.addAttribute("startPage", startPage);
+        model.addAttribute("endPage", endPage);
+        model.addAttribute("num", boards.getTotalElements()- ((page.get()-1)*pageSize ));
         model.addAttribute("boards" , boards);
         return "board/list";
     }
+
+
 
 
     @GetMapping("/form")
